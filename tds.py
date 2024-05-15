625dd6af09473eb63648e252e2c3feb8cb84b279
potrdev <ytbpotr@gmail.com>
2024-05-15 21:56:41 +0200

    1.1

--- tds.py
+++ tds.py
@@ -4,51 +4,87 @@ import random
 #Settings
 winx = 700
 winy = 700
+FPS = 60
+
+#FUNCTIONS
+def randomSpawn():
+  if random.randint(1,2) == 1:
+    return random.randint(-200, -50)
+  else:
+    return random.randint(700, 900)
 
 #PLAYER
 class Player:
   def __init__(self):
     self.x = winx // 2
     self.y = winy // 2
-    self.speed = 0.2
-    
-  def move(self, axis, dir):
-    if axis == "x" and dir:
-      self.x += self.speed
-    elif axis == "x" and not dir:
-      self.x -= self.speed
-    elif axis == "y" and dir:
-      self.y -= self.speed
-    elif axis == "y" and not dir:
-      self.y += self.speed
+    self.speed = 3
+  
+class Enemy:
+  def __init__(self, name, color, hp, x, y, size, speed):
+    self.name = name
+    self.color = color
+    self.hp = hp
+    self.x = x
+    self.y = y
+    self.size = size
+    self.speed = speed
+  
+  def chase(self, player):
+    self.x += (player.x - self.x) * self.speed
+    self.y += (player.y - self.y) * self.speed
             
 
 player = Player()
 
-display = pg.display.set_mode((winx, winy))
+enemies = []
 
+#SPAWNER
+time = 0
+spawnRate = 2
+
+#GAME
+display = pg.display.set_mode((winx, winy))
 game = True
 
 while game:
-  
   display.fill("White")
+  pg.time.Clock().tick(FPS)
+  
+  
+  #SPAWNER
+  if time <= 60 * spawnRate:
+    time += 1
+  else:
+    newEnemy = Enemy("Joze", "green", 3, randomSpawn(), randomSpawn(), 15, 0.005)
+    enemies.append(newEnemy)
+    time = 0
   
   #QUIT
   for event in pg.event.get():
     if event.type == pg.QUIT:
       game = False
-   
+  
   #INPUTS
   keys = pg.key.get_pressed()
   
   if keys[pg.K_w]:
-    player.move("y", True)
+    for e in enemies:
+      e.y += player.speed
   if keys[pg.K_s]:
-    player.move("y", False)
+    for e in enemies:
+      e.y -= player.speed
   if keys[pg.K_a]:
-    player.move("x", False)
+    for e in enemies:
+      e.x += player.speed
   if keys[pg.K_d]:
-    player.move("x", True)
+    for e in enemies:
+      e.x -= player.speed
   
-  pg.draw.circle(display, "yellow", (player.x, player.y), 20)
+  #ENEMIES
+  for enemy in enemies:
+    enemy.chase(player)
+    pg.draw.circle(display, enemy.color, (enemy.x, enemy.y), enemy.size)
+  
+  pg.draw.circle(display, "yellow", (player.x, player.y), 25)
   pg.display.update()
